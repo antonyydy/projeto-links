e78a20b16d332ab54143bef1e651b383d5169f4a
“antonyydy” <email>
2023-09-30 17:32:28 -0400

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  
+  
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/profileZoom.jpeg")
+  } else {
+    img.setAttribute("src", "./assets/profile.jpeg")
+  }
+ 
+}
+
+ 
